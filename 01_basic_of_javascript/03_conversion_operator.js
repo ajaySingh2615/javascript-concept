9b028847829637064b7078451c7b2be0bfd9c129
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-30 14:13:18 +0530

    datatypes conversion

---
+++ 01_basic_of_javascript/03_conversion_operator.js
@@ -0,0 +1,26 @@
+let score = "33";
+
+console.log(typeof score);
+
+let value_in_number = Number(score);
+console.log(typeof value_in_number);
+console.log(value_in_number);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let is_logged_in = 1;
+
+let boolean_is_logged_in = Boolean(1);
+console.log(boolean_is_logged_in);
+console.log(typeof boolean_is_logged_in);
+
+// 1 => true
+// 0 => false
+// "hitesh" => true
+
+let some_number = 33;
+let string_number = String(some_number);
+console.log(typeof string_number);
