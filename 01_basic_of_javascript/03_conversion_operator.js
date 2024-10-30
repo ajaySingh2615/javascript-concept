3f754d3b46a16e363ace583c5a4f7d1e374030e6
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-30 14:35:20 +0530

    conversion operator in javascript

--- 01_basic_of_javascript/03_conversion_operator.js
+++ 01_basic_of_javascript/03_conversion_operator.js
@@ -24,3 +24,43 @@ console.log(typeof boolean_is_logged_in);
 let some_number = 33;
 let string_number = String(some_number);
 console.log(typeof string_number);
+
+console.log("===============OPERATIONS===================");
+
+let value = 3;
+let neg_value = -value;
+console.log(neg_value);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " Ajay";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1);
+
+let game_counter = 100;
+++game_counter;
+// console.log(game_counter);
+
+let x = 3;
+let y = ++x;
+console.log(x);
+console.log(y);
