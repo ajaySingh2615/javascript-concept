9652c517aa98d301fd86ce4ec65564422ac2118e
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-30 14:01:24 +0530

    datatypes in javascript

---
+++ 01_basic_of_javascript/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// alert("hello");  // we are using node.js not browser
+
+let name = "Ajay";
+let age = 18;
+let isLoggedIn = true;
+
+// primitive types
+//
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+//
+// non - primitive types
+// object
+//
+
+console.log(typeof "ajay");
+console.log(typeof null);
+console.log(typeof undefined);
