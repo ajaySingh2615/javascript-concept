f8f34ae56912a72c53df1da3d8770793369ca3a4
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-30 15:46:45 +0530

    data types summary

---
+++ 01_basic_of_javascript/05_data_types_summary.js
@@ -0,0 +1,9 @@
+/*
+    Primitive data types (non-reference type)
+    7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+    
+
+    Reference type (non - primitive types)
+    Array, Objects, Functions
+*/
