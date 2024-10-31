f605cc3953d129d57e1d779478a3ddd73e6b2260
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-31 12:20:09 +0530

    introduction of functions

--- 05_functions/01_intro.js
+++ 05_functions/01_intro.js
@@ -8,4 +8,23 @@ function addTwoNumbers(a, b) {
 
 // sayHello();
 result = addTwoNumbers(2, "2");
-console.log(result);
+// console.log(result);
+
+function calculate_cart_price(...num1) {
+  return num1;
+}
+
+console.log(calculate_cart_price(200, 400, 500));
+
+const user = {
+  username: "hitesh",
+  prices: 99,
+};
+
+function handle_object(anyObject) {
+  console.log(
+    `Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+
+handle_object(user);
