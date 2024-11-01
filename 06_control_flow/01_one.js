0d0561166311bd34fbb9c62a6129737eff51f62f
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-11-01 09:56:13 +0530

    control flow operator - if, if-else, ternary, nullish coalescing  operator and boolean

---
+++ 06_control_flow/01_one.js
@@ -0,0 +1,36 @@
+// if statements
+// <, >, <=, >=, ==, !=, ===, !==
+
+// if (2 == "2") console.log("executed");
+// if (2 === "2") console.log("executed");
+
+// if-else statements
+
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`user power: ${power}`);
+// }
+
+// short hand notation
+
+// const balance = 1000;
+// if (balance > 500) console.log("test");
+
+// nested if-else
+// switch statements
+
+// falsy values
+// false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// Nullish Coalescing Operator (??) (null or undefined)
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+val1 = undefined ?? 20;
+console.log(val1);
+
+// ternary operator
